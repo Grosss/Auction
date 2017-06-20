68e8f355a4d3fb4ccf09d5281ab3e3d2cc1b7b06
Grosss <artemenko,nickolai@gmail.com>
2017-06-20 14:53:18 +0200

    providers

---
+++ MvcPL/Providers/CustomMembershipProvider.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+using BLL.Interface.Entities;
+using BLL.Interface.Services;
+
+namespace MvcPL.Providers
+{
+    public class CustomMembershipProvider : MembershipProvider
+    {
+        public IUserService UserService
+            => (IUserService)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IUserService));
+
+        public IRoleService RoleService
+            => (IRoleService)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IRoleService));
+
+        public MembershipUser CreateUser(string email, string login, string password, string firstName, string lastName, decimal money = 0)
+        {
+            MembershipUser membershipUser = GetUser(login, false);
+
+            if (ReferenceEquals(membershipUser, null))
+                return null;
+
+            var user = new UserEntity
+            { 
+                Email = email,
+                Login = login,
+                Password = Crypto.HashPassword(password),
+                FirstName = firstName,
+                LastName = lastName,
+                Money = money                
+            };
+            
+            UserService.CreateUser(user);
+            membershipUser = GetUser(login, false);
+            return membershipUser;
+        }
+
+        public override MembershipUser GetUser(string login, bool userIsOnline)
+        {
+            var user = UserService.GetUserByLogin(login);
+
+            if (ReferenceEquals(user, null))
+                return null;
+
+            var memberUser = new MembershipUser("CustomMembershipProvider", user.Login,
+                null, user.Email, null, null,
+                false, false, DateTime.Now,
+                DateTime.MinValue, DateTime.MinValue,
+                DateTime.MinValue, DateTime.MinValue);
+
+            return memberUser;
+        }
+
+        public override bool ValidateUser(string login, string password)
+        {
+            var user = UserService.GetUserByLogin(login);
+
+            if (!ReferenceEquals(user, null) && Crypto.VerifyHashedPassword(user.Password, password))
+                return true;
+            else
+                return false;
+        }
+
+        #region Stubs
+
+        public override bool EnablePasswordRetrieval
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool EnablePasswordReset
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool RequiresQuestionAndAnswer
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int MaxInvalidPasswordAttempts
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int PasswordAttemptWindow
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool RequiresUniqueEmail
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override MembershipPasswordFormat PasswordFormat
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int MinRequiredPasswordLength
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int MinRequiredNonAlphanumericCharacters
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override string PasswordStrengthRegularExpression
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override MembershipUser CreateUser(string login, string password, string email, string passwordQuestion, string passwordAnswer, bool isApproved, object providerUserKey, out MembershipCreateStatus status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePasswordQuestionAndAnswer(string username, string password, string newPasswordQuestion, string newPasswordAnswer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string GetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool ChangePassword(string username, string oldPassword, string newPassword)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ResetPassword(string username, string answer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UpdateUser(MembershipUser user)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public override bool UnlockUser(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUser GetUser(object providerUserKey, bool userIsOnline)
+        {
+            throw new NotImplementedException();
+        }        
+
+        public override string GetUserNameByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool DeleteUser(string username, bool deleteAllRelatedData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection GetAllUsers(int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetNumberOfUsersOnline()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByName(string usernameToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override MembershipUserCollection FindUsersByEmail(string emailToMatch, int pageIndex, int pageSize, out int totalRecords)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion 
+    }
+}
---
+++ MvcPL/Providers/CustomRoleProvider.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+using BLL.Interface.Entities;
+using BLL.Interface.Services;
+
+namespace MvcPL.Providers
+{
+    public class CustomRoleProvider : RoleProvider
+    {
+        public IUserService UserService
+           => (IUserService)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IUserService));
+
+        public IRoleService RoleService
+            => (IRoleService)System.Web.Mvc.DependencyResolver.Current.GetService(typeof(IRoleService));
+                
+        public override bool IsUserInRole(string login, string roleName)
+        {
+            var user = UserService.GetUserByLogin(login);
+
+            if (ReferenceEquals(user, null))
+                return false;
+
+            return RoleService.GetUserRoles(user.Id).Any(role => role.Name == roleName);
+        }
+
+        public override string[] GetRolesForUser(string login)
+        {
+            var user = UserService.GetUserByLogin(login);
+
+            var entityRoles = RoleService.GetUserRoles(user.Id);
+
+            string[] roles = new string[entityRoles.Count()];
+
+            for (int i = 0; i < entityRoles.Count(); i++)
+                roles[i] = entityRoles.ElementAt(i).Name;
+
+            return roles;
+        }
+
+        public override void CreateRole(string roleName)
+        {
+            var newRole = new RoleEntity() { Name = roleName };
+            RoleService.CreateRole(newRole);
+        }
+
+        #region Stubs
+
+        public override string ApplicationName
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool DeleteRole(string roleName, bool throwOnPopulatedRole)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool RoleExists(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AddUsersToRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveUsersFromRoles(string[] usernames, string[] roleNames)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetUsersInRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] GetAllRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string[] FindUsersInRole(string roleName, string usernameToMatch)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
